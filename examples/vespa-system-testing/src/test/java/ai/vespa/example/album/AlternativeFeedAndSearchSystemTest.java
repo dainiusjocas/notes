ae701085601ae2481f97575b4f23ca285cf91710
Dainius Jocas <djocas@ravenpack.com>
2026-02-18 21:53:03 +0100

    naming

--- examples/vespa-system-testing/src/test/java/ai/vespa/example/album/AlternativeFeedAndSearchSystemTest.java
+++ examples/vespa-system-testing/src/test/java/ai/vespa/example/album/AlternativeFeedAndSearchSystemTest.java
@@ -1,25 +1,27 @@
 package ai.vespa.example.album;
 
 import ai.vespa.example.album.systemtests.BaseSystemTest;
-import org.assertj.core.util.Lists;
+import ai.vespa.hosted.cd.SystemTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 
-public class FeedAndSearchSystemTestsAlternative extends BaseSystemTest {
+@SystemTest
+public class AlternativeFeedAndSearchSystemTest extends BaseSystemTest {
+
     @Test
     void feedAndSearch() {
         var yql = "SELECT * FROM music WHERE artist CONTAINS 'coldplay'";
-        given("All documents are deleted from the cluster")
+        given("all documents are deleted from the cluster")
                 .deleteAll("music")
                 .expectDeleted();
         when("searching for music")
                 .search("yql", yql, "timeout", "10s");
         then("no documents are found")
                 .status(200)
-                .body("/root/fields/totalCount", equalTo(0));
+                .body("/root/fields/totalCount", equalTo(0L));
 
         when("a document is fed and searched")
                 .feed("music", "test1",
@@ -28,7 +30,7 @@ public class FeedAndSearchSystemTestsAlternative extends BaseSystemTest {
                 .and()
                 .search("yql", yql, "timeout", "10s");
         then("1 document is found")
-                .body("/root/fields/totalCount", equalTo(1));
+                .body("/root/fields/totalCount", equalTo(1L));
     }
 
     @Override
