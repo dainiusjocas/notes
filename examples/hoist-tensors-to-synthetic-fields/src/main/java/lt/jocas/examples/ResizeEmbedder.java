4a8b2685ff48fa2ea46d32abdbf0abe6a627d2f7
Dainius Jocas <dainius.jocas@gmail.com>
2026-08-07 00:13:00 +0300

    link

--- examples/hoist-tensors-to-synthetic-fields/src/main/java/lt/jocas/examples/ResizeEmbedder.java
+++ examples/hoist-tensors-to-synthetic-fields/src/main/java/lt/jocas/examples/ResizeEmbedder.java
@@ -1,4 +1,19 @@
 package lt.jocas.examples;
 
-public class ResizeEmbedder {
+import com.yahoo.language.process.Embedder;
+import com.yahoo.tensor.Tensor;
+import com.yahoo.tensor.TensorType;
+
+import java.util.List;
+
+public class ResizeEmbedder implements Embedder {
+    @Override
+    public List<Integer> embed(String text, Context context) {
+        return List.of();
+    }
+
+    @Override
+    public Tensor embed(String text, Context context, TensorType tensorType) {
+        return null;
+    }
 }
