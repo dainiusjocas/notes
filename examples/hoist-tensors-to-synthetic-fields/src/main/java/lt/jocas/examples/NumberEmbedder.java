aa947b0593d383240442c99f3a8cdc0b94b562ee
Dainius Jocas <dainius.jocas@gmail.com>
2026-08-08 11:29:24 +0300

    refactor

--- examples/hoist-tensors-to-synthetic-fields/src/main/java/lt/jocas/examples/NumberEmbedder.java
+++ examples/hoist-tensors-to-synthetic-fields/src/main/java/lt/jocas/examples/NumberEmbedder.java
@@ -4,7 +4,6 @@ import com.yahoo.language.process.Embedder;
 import com.yahoo.tensor.Tensor;
 import com.yahoo.tensor.TensorType;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class NumberEmbedder implements Embedder {
@@ -16,20 +15,16 @@ public class NumberEmbedder implements Embedder {
     /**
      * Converts an array of numbers into a mapped tensor.
      * @param input string is a list of number that can be parsed to double separated by comma.
-     * @param context the context which may influence an embedder's behavior
+     * @param context the context that may influence an embedder's behavior
      * @param tensorType the type of the tensor to be returned
-     * @return
+     * @return Tensor of tensorType
      */
     @Override
     public Tensor embed(String input, Context context, TensorType tensorType) {
-        Tensor.Builder builder = Tensor.Builder.of(tensorType);
-        List<Double> numbers = Arrays.stream(input.split(","))
-                .map(String::trim)
-                .map(Double::parseDouble)
-                .toList();
-        for (int offset = 0; offset < numbers.size(); offset++) {
-            builder.cell(numbers.get(offset), offset);
-        }
+        var builder = Tensor.Builder.of(tensorType);
+        var values = List.of(input.split(","));
+        for (int offset = 0; offset < values.size(); offset++)
+            builder.cell(Double.parseDouble(values.get(offset).trim()), offset);
         return builder.build();
     }
 }
